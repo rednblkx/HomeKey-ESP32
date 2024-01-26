ffaf540528370778ab3826aa606ea90a78f9b630
White Red <dannegru40@gmail.com>
2024-01-27 01:25:35 +0200

    should fix the crash and some potential memory issues #8

--- src/auth/authContext.h
+++ src/auth/authContext.h
@@ -20,22 +20,22 @@ class AuthenticationContext
 protected:
   const char *TAG = "HKAuthCtx";
   homeKeyReader::readerData_t *readerData;
-  std::vector<uint8_t> *readerEphX;
-  std::vector<uint8_t> *readerEphPrivKey;
-  std::vector<uint8_t> *readerEphPubKey;
-  std::vector<uint8_t> *endpointEphPubKey;
-  std::vector<uint8_t> *endpointEphX;
+  std::vector<uint8_t> readerEphX;
+  std::vector<uint8_t> readerEphPrivKey;
+  std::vector<uint8_t> readerEphPubKey;
+  std::vector<uint8_t> endpointEphPubKey;
+  std::vector<uint8_t> endpointEphX;
   PN532 *nfc;
-  std::vector<uint8_t> *transactionIdentifier;
-  std::vector<uint8_t> *readerIdentifier;
-  std::tuple<std::vector<uint8_t> *, std::vector<uint8_t> *> generateEphemeralKey();
+  std::vector<uint8_t> transactionIdentifier;
+  std::vector<uint8_t> readerIdentifier;
+  std::tuple<std::vector<uint8_t>, std::vector<uint8_t>> generateEphemeralKey();
   std::vector<uint8_t> signSharedInfo(uint8_t *stdTlv, size_t len);
-  std::vector<uint8_t> *get_x(std::vector<uint8_t> *pubKey);
-  std::vector<uint8_t> *get_x(uint8_t *pubKey, size_t len);
+  std::vector<uint8_t> get_x(std::vector<uint8_t> &pubKey);
+  std::vector<uint8_t> get_x(uint8_t *pubKey, size_t len);
   void Auth0_keying_material(const char *context, uint8_t ePub_X[32], uint8_t *keyingMaterial, uint8_t *out, size_t outLen);
   void Auth1_keys_generator(uint8_t *persistentKey, uint8_t *volatileKey);
   void get_shared_key(uint8_t *outBuf, size_t oLen);
-  issuerEndpoint::issuerEndpoint_t *find_endpoint_by_cryptogram(std::vector<uint8_t> cryptogram);
+  issuerEndpoint::issuerEndpoint_t *find_endpoint_by_cryptogram(std::vector<uint8_t>& cryptogram);
   void Auth1_keying_material(uint8_t *keyingMaterial, const char *context, uint8_t *out, size_t outLen);
 
 public:
@@ -43,5 +43,4 @@ public:
   std::tuple<issuerEndpoint::issuerEndpoint_t *, DigitalKeySecureContext *, std::vector<uint8_t>, homeKeyReader::KeyFlow> std_auth();
   AuthenticationContext(PN532 *nfc, homeKeyReader::readerData_t *readerData);
   AuthenticationContext(const AuthenticationContext &);
-  ~AuthenticationContext();
 };
