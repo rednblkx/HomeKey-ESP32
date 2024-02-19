40b31d58c969438663a6accc4641482939b747ff
White Red <rednblkx@protonmail.com>
2024-02-19 17:12:20 +0200

    moved std flow into a class

--- src/auth/hkAuthContext.h
+++ src/auth/hkAuthContext.h
@@ -1,24 +1,13 @@
 #pragma once
 #include <tuple>
-#include <esp_random.h>
-#include <util/utils.h>
-#include <mbedtls/ecp.h>
-#include <mbedtls/entropy.h>
-#include <mbedtls/ctr_drbg.h>
-#include "PN532.h"
 #include <util/BERTLV.h>
-#include <mbedtls/hkdf.h>
-#include <mbedtls/md.h>
-#include <mbedtls/ecdsa.h>
-#include <mbedtls/ecdh.h>
 #include "HomeKey.h"
 #include <util/DigitalKeySecureContext.h>
-#include <util/x963kdf.h>
-#include <esp32-hal.h>
 #include <nvs.h>
 #include <util/CommonCryptoUtils.h>
 #include <chrono>
 #include <auth/hkFastAuth.h>
+#include <auth/hkStdAuth.h>
 
 class HKAuthenticationContext : CommonCryptoUtils
 {
@@ -33,10 +22,7 @@ private:
   bool (*nfcInDataExchange)(uint8_t *data, size_t lenData, uint8_t *res, uint8_t *resLen){};
   std::vector<uint8_t> transactionIdentifier;
   std::vector<uint8_t> readerIdentifier;
-  void Auth1_keys_generator(uint8_t *persistentKey, uint8_t *volatileKey);
-  void Auth1_keying_material(uint8_t *keyingMaterial, const char *context, uint8_t *out, size_t outLen);
   std::vector<uint8_t> commandFlow(homeKeyReader::CommandFlowStatus status);
-  std::tuple<homeKeyIssuer::issuer_t *, homeKeyEndpoint::endpoint_t *, DigitalKeySecureContext, std::vector<uint8_t>, homeKeyReader::KeyFlow> std_auth();
 
 public:
   HKAuthenticationContext(bool (*nfcInDataExchange)(uint8_t *data, size_t lenData, uint8_t *res, uint8_t *resLen), homeKeyReader::readerData_t &readerData);
