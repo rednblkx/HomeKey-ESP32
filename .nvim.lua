ba0fc9e4cb7147275152953fe2573c69f6082cdd
White Red <rednblkx@protonmail.com>
2025-01-11 17:49:50 +0200

    fix: clangd

--- .nvim.lua
+++ .nvim.lua
@@ -2,7 +2,14 @@ require("lspconfig").clangd.setup({
 	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 	cmd = {
 		os.getenv("HOME") .. "/esp-clang/bin/clangd",
+		"--query-driver="
+			.. os.getenv("HOME")
+			.. "/.espressif/tools/xtensa-esp-elf/**/xtensa-esp-elf/bin/xtensa-*-elf-*,"
+			.. os.getenv("HOME")
+			.. "/tools/riscv32-esp-elf/**/riscv32-esp-elf/bin/riscv32-esp-elf-*",
 		"--background-index",
+		"--import-insertions",
+		"--all-scopes-completion",
 	},
 	on_attach = require("cmp_nvim_lsp").on_attach,
 	lsp_flags = require("cmp_nvim_lsp").lsp_flags,
