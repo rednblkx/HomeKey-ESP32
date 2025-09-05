ef7d910ffa59aee627e7d4aa7976b20ae54e38c1
White Red <rednblkx@protonmail.com>
2025-09-05 22:27:18 +0300

    chore: ide setup

--- .nvim.lua
+++ .nvim.lua
@@ -1,7 +1,6 @@
 require("lspconfig").clangd.setup({
 	cmd = {
 		os.getenv("HOME") .. "/.espressif/tools/esp-clang/esp-19.1.2_20250312/esp-clang/bin/clangd",
-		"--header-insertion=iwyu",
 		"--query-driver="
 			.. os.getenv("HOME")
 			.. "/.espressif/tools/xtensa-esp-elf/**/xtensa-esp-elf/bin/xtensa-*-elf-*,"
@@ -10,9 +9,6 @@ require("lspconfig").clangd.setup({
 		"--background-index",
 		"--suggest-missing-includes",
 		"--clang-tidy",
-		"--completion-style=detailed",
-		"--function-arg-placeholders",
-		"--fallback-style=llvm",
 	},
 	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 })
