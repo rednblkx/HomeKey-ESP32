af4e48290ddda02e175bebb037c3e883aba7f526
White Red <rednblkx@protonmail.com>
2025-10-29 22:37:17 +0200

    fix(ui): duplicate icons

--- data/src/lib/components/AppOTA.svelte
+++ data/src/lib/components/AppOTA.svelte
@@ -513,9 +513,6 @@
 					{#if uploading && currentUploadType === 'firmware'}
 						<span class="loading loading-spinner loading-sm"></span>
 					{/if}
-					<svg class:loading={uploading && currentUploadType === 'firmware'} xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-					</svg>
 					{(uploading && currentUploadType === 'firmware') ? 'Uploading...' : 'Upload Firmware'}
 				</button>
 
@@ -527,9 +524,6 @@
 					{#if uploading && currentUploadType === 'littlefs'}
 						<span class="loading loading-spinner loading-sm"></span>
 					{/if}
-					<svg class:loading={uploading && currentUploadType === 'littlefs'} xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2H5a2 2 0 00-2-2z" />
-					</svg>
 					{(uploading && currentUploadType === 'littlefs') ? 'Uploading...' : 'Upload LittleFS'}
 				</button>
 
@@ -541,9 +535,6 @@
 					{#if uploading && sequentialUpload}
 						<span class="loading loading-spinner loading-sm"></span>
 					{/if}
-					<svg class:loading={uploading && sequentialUpload} xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-					</svg>
 					{(uploading && sequentialUpload) ? uploadStepText() : 'Upload Both'}
 				</button>
 			</div>
