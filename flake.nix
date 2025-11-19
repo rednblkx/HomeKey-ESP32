0aa80a9623c85e321c4237839d0bdd549a03c30d
White Red <rednblkx@protonmail.com>
2025-11-19 19:38:06 +0200

    chore: nix flake devshell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs-esp-dev = {
+      url = "github:mirrexagon/nixpkgs-esp-dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-esp-dev,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        bun
+        pkgsi686Linux.glibc
+        nixpkgs-esp-dev.packages.${system}.esp-idf-full
+      ];
+
+      shellHook = ''
+        export CLANGD_FLAGS="--query-driver=`which riscv32-esp-elf-g++`"
+      '';
+    };
+  };
+}
