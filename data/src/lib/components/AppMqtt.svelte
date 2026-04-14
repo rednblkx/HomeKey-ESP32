b27ad9dc6d272036e3d69bbafd1eb34bdaceb857
White Red <rednblkx@protonmail.com>
2026-04-14 18:43:03 +0300

    chore(webui): mqtt tls cn check disable option

--- data/src/lib/components/AppMqtt.svelte
+++ data/src/lib/components/AppMqtt.svelte
@@ -563,8 +563,8 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
                   </svg>
                   <div>
-                    <p class="font-medium text-sm">Skip Certificate Validation</p>
-                    <p class="text-xs text-error">Not recommended for production</p>
+                    <p class="font-medium text-sm">Skip CN validation</p>
+                    <p class="text-xs text-error">Leave it disabled if you don't know</p>
                   </div>
                 </div>
                 <input
