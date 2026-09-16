590fc4ae355baab1490183779020b10319fafbb0
White Red <rednblkx@protonmail.com>
2026-09-16 17:01:21 +0300

    build: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    esp-dev.url = "github:rednblkx/nixpkgs-esp-dev";
   };
   outputs = {
     self,
@@ -11,12 +11,22 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
+      overlays = [ esp-dev.overlays.default ];
+      # The Python library ecdsa is marked as insecure, but we need it for
+      # esptool. See https://github.com/mirrexagon/nixpkgs-esp-dev/issues/109
+      config.permittedInsecurePackages = [
+        "python3.13-ecdsa-0.19.1"
+      ];
+    };
+    esp-idf-v5_5_5 = esp-dev.packages.${system}.esp-idf-full.override {
+      rev = "b774170ff46c393eeb5e495ea37936038d3f4f4f";
+      sha256 = "1ywzyw34qrj147lgqy89s39m3sg604mazjk7485m6p36qyx6q5gz";
     };
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         bun
-        esp-dev.packages.${system}.esp-idf-full
+        esp-idf-v5_5_5
       ];
 
       shellHook = ''
